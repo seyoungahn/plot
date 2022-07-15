98b319222eee3821c94f3c744ab2a6df7827f7f2
Seyoung Ahn <67622476+seyoungahn@users.noreply.github.com>
2022-07-15 21:59:46 +0900

    220715 modified

--- ha2.py
+++ ha2.py
@@ -12,11 +12,12 @@ def plot_CDF(filename):
     params = list(map(int, params))
 
     n_UEs = params[1]
+    epochs = 100
 
-    methods = ['Non-cooperative MRT greedy', 'Non-cooperative MRT random', 'Non-cooperative ZF greedy', 'Non-cooperative ZF random', 'Cooperative MRT greedy', 'Cooperative MRT random', 'Cooperative ZF greedy', 'Cooperative ZF random']
+    methods = ['Non-cooperative MRT random', 'Non-cooperative MRT greedy', 'Non-cooperative ZF random', 'Non-cooperative ZF greedy', 'Cooperative MRT random', 'Cooperative MRT greedy', 'Cooperative ZF random', 'Cooperative ZF greedy']
     columns = []
-    for i in range(n_UEs):
-        columns.append("UE #{}".format(i+1))
+    for i in range(n_UEs * epochs):
+        columns.append(i+1)
     values = []
 
     with open('./data/' + filename, 'r', encoding='utf-8') as f:
@@ -31,25 +32,24 @@ def plot_CDF(filename):
     # print(np.shape(values))
 
     fontsize = 10
-    msize = 5
-    lwidth = 1
+    msize = 4
+    lwidth = 2
     N_markers = 10
-    marker_sep = round(n_UEs/N_markers)
-    y_axis = np.arange(0, n_UEs+1)/n_UEs
+    marker_sep = round(n_UEs * epochs/N_markers)
+    y_axis = np.arange(1, n_UEs * epochs + 1)/(n_UEs * epochs)
 
     ## MRT / ZF: r / k
     ## Random / greedy: > / s
-    ## Non-cooperative / Cooperative: - / --
+    ## Non-cooperative / Cooperative: - / *
     # 'Non-cooperative MRT greedy', 'Non-cooperative MRT random', 'Non-cooperative ZF greedy', 'Non-cooperative ZF random',
     # 'Cooperative MRT greedy', 'Cooperative MRT random', 'Cooperative ZF greedy', 'Cooperative ZF random'
-    type = ['rs-', 'r>-', 'ks-', 'k>-', 'rs--', 'r>--', 'ks--', 'k>--']
+    type = ['rs-', 'r>-', 'ks-', 'k>-', 'rs:', 'r>:', 'ks:', 'k>:']
 
     for i in range(8):
-        temp = np.append(values[i], 0.0)
-        temp.sort()
-        plt.plot(temp, y_axis, type[i], lw=lwidth, ms=msize, markevery=marker_sep, label=methods[i])
+        values[i].sort()
+        plt.plot(values[i], y_axis, type[i], lw=lwidth, ms=msize, markevery=marker_sep, label=methods[i])
     plt.title(title)
-    plt.xlim(left=0)
+    plt.xlim((0.0, 9.0))
     plt.ylim((0.0, 1.0))
     plt.xticks(fontsize=fontsize-2)
     plt.yticks(fontsize=fontsize-2)
@@ -64,7 +64,8 @@ if __name__ == '__main__':
     import os
     path_dir = './data'
     file_list = os.listdir(path_dir)
-    file_list = file_list[1:]
+    # file_list = file_list[1:]
+    print(file_list)
     # plot_CDF("TM50_K10_Q1_N16_tau15.csv")
 
     for i in tqdm.tqdm(range(len(file_list)), desc='Plotting CDF'):
